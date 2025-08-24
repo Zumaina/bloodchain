a561044bc1d1b91fcf51790f84de00db93a1a439
Rahnuma <mahjabin3619@gmail.com>
2025-08-24 17:17:12 +0600

    Fixed an error

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import { Link } from "react-router-dom";
-import logo1 from "../assets/logo1.png";   // ✅ correct path & name
 
 const Navbar = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: "#b71c1c" }}>
       <Toolbar>
-        <Box
-          component="img"
-          src={logo1}   // ✅ must match import
-          alt="App Logo"
-          sx={{ height: 40, marginRight: 1 }}
-        />
         <Typography
           variant="h6"
           component={Link}
@@ -31,7 +24,9 @@ const Navbar = () => {
           <Button color="inherit" component={Link} to="/">
             Home
           </Button>
-          <Button color="inherit">Donate</Button>
+          <Button color="inherit" component={Link} to="/donate">
+            Donate
+          </Button>
           <Button color="inherit" component={Link} to="/request">
             Request
           </Button>
